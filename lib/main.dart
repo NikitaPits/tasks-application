6be5c22e2da13f3f1e17fd45ba1afffe3156540e
nikitapits <pitsnikita@gmail.com>
2023-07-05 17:17:47 +0300

    created task router

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:task_list_app/home_page.dart';
+import 'package:task_list_app/pages/tasks/_view/tasks_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomePage();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'tasks',
+          builder: (BuildContext context, GoRouterState state) {
+            return const TasksPage();
+          },
+        ),
+      ],
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Task list App',
+      routerConfig: _router,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: HomePage(),
     );
   }
 }
